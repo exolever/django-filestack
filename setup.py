468c607bf70ed8fb95c5673f4cb72a1b12eb28b2
Tomas <tomasgarzonhervas@gmail.com>
2019-03-25 09:33:45 +0100

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django_filestack',
-    version='0.1.1',
+    version='0.2.0',
     description="""Add Model support for Filestack files""",
     long_description=readme + '\n\n' + history,
     author='Tomas',
